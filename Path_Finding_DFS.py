5673b3e5894547a67f0ccd3a25b972bffb074c6e
Soumya <soumyawork15@gmail.com>
2023-10-13 10:51:58 +0100

    Update Path_Finding_DFS.py

--- Path_Finding_DFS.py
+++ Path_Finding_DFS.py
@@ -7,12 +7,31 @@ graph = {
     'K' : []
 }
 
+# Define a function to check if there is a path between two nodes in a graph using DFS.
+# Parameters:
+# - graph: A dictionary representing the directed graph.
+# - source: The current node being checked.
+# - destination: The target node to find a path to.
+
 def is_path_dfs(graph, source, destination):
+    # Base case: If the current node is the destination, return True indicating a path is found.
     if source == destination:
         return True
+
+    # Recursive case: Check each neighbor of the current node.
     for neighbor in graph[source]:
+        # Recursively call the function for each neighbor.
         if is_path_dfs(graph, neighbor, destination):
-            return print('Path is there')
+            return True  # Print a message if a path is found.
+
+    # If the loop completes without finding a path, return False.
     return False
 
-is_path_dfs(graph, 'F', 'K')
+# Call the is_path_dfs function with a sample graph and source-destination nodes.
+result = is_path_dfs(graph, 'F', 'K')
+
+# Check the result and print a message.
+if result:
+    print('Path exists')
+else:
+    print('Path is not there')
